eec7be2aee5ff76b21ee11cc640ae57ae7864771
RMo <morici23@gmail.com>
2022-05-12 13:52:09 -0400

    Add Bullets to the profile model as an embedded document.

--- server/models/profile.js
+++ server/models/profile.js
@@ -1,8 +1,18 @@
 import mongoose from 'mongoose'
 
+const Bullets = new mongoose.Schema({
+  text: String
+})
+
 const profileSchema = new mongoose.Schema({
-  email: {type: String, required: true, lowercase: true, unique: true},
+  email: { 
+    type: String, 
+    required: true, 
+    lowercase: true, 
+    unique: true
+  },
   name: String,
+  bullets: [Bullets]
 },{
     timestamps: true,
 })
