28bc1749afee94741de375015c777642ac3c599d
RMo <morici23@gmail.com>
2022-05-12 17:07:25 -0400

    Get bullets into state via fetch request.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react'
+import * as bulletService from '../services/bulletService'
+
 const Home = ({user, handleLogout}) => {
+  const [bullets, setBullets] = useState([])
+
+  useEffect(()=> {
+    try {
+      const fetchBullets = async () => {
+        const bullets = await bulletService.getAllBullets()
+        setBullets(bullets)
+      }
+      fetchBullets()
+    } catch(error) {
+      console.log(error)
+    }
+  }, [user])
+
   return (
     <>
       <button onClick={handleLogout}>LOGOUT</button>
+
     </>
   );
 }
---
+++ client/src/services/bulletService.js
@@ -0,0 +1,13 @@
+import * as tokenService from '../services/tokenService'
+const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/bullets`
+
+const getAllBullets = async () => {
+  const res = await fetch(BASE_URL, {
+    headers: {Authorization: `Bearer ${tokenService.getToken()}`}
+  })
+  return await res.json()
+}
+
+export {
+  getAllBullets
+}
